70a07f30994b56c2738e257efc69e449d6998399
jlescano-mobup <jlescano@mobuptacna.com>
2024-09-20 16:50:56 -0500

    Agregar archivos de proyecto.

---
+++ Context/MyDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API_Rest_CP1_C4.Models;
+
+namespace API_Rest_CP1_C4.Context
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
