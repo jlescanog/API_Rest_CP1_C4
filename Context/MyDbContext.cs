1da3d838d94712bbda5a685e27a6f2eed8c6fc23
jlescanog <jlescanoguevara@gmail.com>
2024-10-01 17:02:13 -0500

    Se corrigen, MyDbContext y se agrega servidor sql nuevo

--- Context/MyDbContext.cs
+++ Context/MyDbContext.cs
@@ -3,9 +3,9 @@ using API_Rest_CP1_C4.Models;
 
 namespace API_Rest_CP1_C4.Context
 {
-    public class MyDbContext : DbContext
-    {
-        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
-        public DbSet<Producto> Productos { get; set; }
-    }
+	public class MyDbContext : DbContext
+	{
+		public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+		public DbSet<Producto> Productos { get; set; }
+	}
 }
